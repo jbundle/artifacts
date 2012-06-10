6b1eed75558803ecd6b1d64cf97dc2d4ee5199d0
Don Corley <don@donandann.com>
2012-06-10 14:14:11 -0700

    General cleanup

--- src/main/resources/dev/karaf-setup.sh
+++ src/main/resources/dev/karaf-setup.sh
@@ -1,12 +1,12 @@
 export KARAF_DIST=apache-karaf-full-3.0.0-SNAPSHOT
 export KARAF_DIST=apache-servicemix-4.4.1-fuse-01-06
-export KARAF_DIST=apache-karaf-2.2.7
+KARAF_DIST=apache-karaf-2.2.7
 # export KARAF_DIST=apache-servicemix-4.4.1
-export KARAF_DIST=apache-servicemix-4.4.2
+# export KARAF_DIST=apache-servicemix-4.4.2
 
 export KARAF_LOCATION=~/workspace-karaf/karaf/assemblies/apache-karaf-full/target/apache-karaf-full-3.0.0-SNAPSHOT-bin.tar.gz
 export KARAF_LOCATION=/space/download/software/dev/osgi/karaf/$KARAF_DIST.tar.gz
-export KARAF_LOCATION=/space/download/software/dev/soa/servicemix/$KARAF_DIST.tar.gz
+# export KARAF_LOCATION=/space/download/software/dev/soa/servicemix/$KARAF_DIST.tar.gz
 
 export DEV_LOCATION=~/workspace/workspace
 export DEV_LOCATION=/space/download/workspace
@@ -45,6 +45,6 @@ export JAVA_MAX_PERM_MEM=512M
 
 echo Remember to increase the memory params before restarting karaf
 
-# bin/karaf $1
-bin/servicemix $1
+bin/karaf $1
+# bin/servicemix $1
 
