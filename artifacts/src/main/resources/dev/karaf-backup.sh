6f643f6b4b1292d499fc8e167972251c9a48bd92
Don Corley <don@donandann.com>
2012-08-28 15:35:55 -0700

    Update the scripts to using karaf 2.2.9

--- artifacts/src/main/resources/dev/karaf-backup.sh
+++ artifacts/src/main/resources/dev/karaf-backup.sh
@@ -1,4 +1,4 @@
-KARAF_DIST=apache-karaf-2.2.8
+KARAF_DIST=apache-karaf-2.2.9
 cd /usr/local/java/web
 rm -fr $KARAF_DIST.bu
 cp -r $KARAF_DIST $KARAF_DIST.bu
--- artifacts/src/main/resources/dev/karaf-restore.sh
+++ artifacts/src/main/resources/dev/karaf-restore.sh
@@ -1,4 +1,4 @@
-KARAF_DIST=apache-karaf-2.2.8
+KARAF_DIST=apache-karaf-2.2.9
 cd /usr/local/java/web
 rm -fr $KARAF_DIST
 cp -r $KARAF_DIST.bu $KARAF_DIST
