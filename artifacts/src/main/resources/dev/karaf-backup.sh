cce051269d5f0e8db69d47eb055ef4aff8f69f61
Don Corley <don@donandann.com>
2013-03-04 08:57:14 -0800

    Fix backup/restore macros

--- artifacts/src/main/resources/dev/karaf-backup.sh
+++ artifacts/src/main/resources/dev/karaf-backup.sh
@@ -1,4 +1,4 @@
-KARAF_DIST=apache-karaf-2.2.9
+KARAF_DIST=apache-karaf-2.3.0
 cd /usr/local/java/web
 rm -fr $KARAF_DIST.bu
 cp -r $KARAF_DIST $KARAF_DIST.bu
--- artifacts/src/main/resources/dev/karaf-restore.sh
+++ artifacts/src/main/resources/dev/karaf-restore.sh
@@ -1,4 +1,4 @@
-KARAF_DIST=apache-karaf-2.2.9
+KARAF_DIST=apache-karaf-2.3.0
 cd /usr/local/java/web
 rm -fr $KARAF_DIST
 cp -r $KARAF_DIST.bu $KARAF_DIST
