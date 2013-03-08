04c8c88441da51098d2206325b525d2d2a22b9bd
Don Corley <don@donandann.com>
2013-03-08 11:14:47 -0800

    update karaf references to 2.3.1

--- artifacts/src/main/resources/dev/karaf-backup.sh
+++ artifacts/src/main/resources/dev/karaf-backup.sh
@@ -1,4 +1,4 @@
-KARAF_DIST=apache-karaf-2.3.0
+KARAF_DIST=apache-karaf-2.3.1
 cd /usr/local/java/web
 rm -fr $KARAF_DIST.bu
 cp -r $KARAF_DIST $KARAF_DIST.bu
--- artifacts/src/main/resources/dev/karaf-restore.sh
+++ artifacts/src/main/resources/dev/karaf-restore.sh
@@ -1,4 +1,4 @@
-KARAF_DIST=apache-karaf-2.3.0
+KARAF_DIST=apache-karaf-2.3.1
 cd /usr/local/java/web
 rm -fr $KARAF_DIST
 cp -r $KARAF_DIST.bu $KARAF_DIST
