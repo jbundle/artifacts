aa64dcd0b55a17ed04ae3c86c16fbd73a3c62164
Don Corley <don@donandann.com>
2020-01-19 12:41:04 -0800

    Automate external dependency handling

--- artifacts/src/main/resources/bindex/bindex-local.sh
+++ artifacts/src/main/resources/bindex/bindex-local.sh
@@ -8,6 +8,7 @@ export TOURGEEK_VERSION=0.8.0
 export JBUNDLE_SNAPSHOT_VERSION=0.8.0-SNAPSHOT
 export TOURGEEK_SNAPSHOT_VERSION=0.8.0-SNAPSHOT
 export REPO_VERSION=1.0.3
+mkdir ~/temp
 cd ~/temp
 
 # ---------------- external -----------------
@@ -24,69 +25,67 @@ sync_package () {
   rsync -r $REPOSITORY/$PACKAGE/$VERSION $PACKAGE
 }
 
+#PACKAGE=mysql/mysql-connector-java
+#VERSION=5.1.48
+#sync_package
+#
+#PACKAGE=biz/source_code/base64coder
+#VERSION=/2010-12-19
+#sync_package
+
+#PACKAGE=javax/mail/mail
+#VERSION=1.4.4
+#sync_package
+
+#PACKAGE=org/json/json
+#VERSION=20190722
+#sync_package
+#
+#PACKAGE=org/apache/derby/derby
+#VERSION=10.14.2.0
+#sync_package
+
+#PACKAGE=commons-collections/commons-collections
+#VERSION=3.2.1
+#sync_package
+#
+#PACKAGE=jakarta/xml/soap/jakarta.xml.soap-api
+#VERSION=1.4.1
+#sync_package
+#
+#PACKAGE=com/sun/xml/messaging/saaj/saaj-impl
+#VERSION=1.5.1
+#sync_package
+#
+#PACKAGE=jakarta/activation/jakarta.activation-api
+#VERSION=1.2.1
+#sync_package
+#
+#PACKAGE=com/sun/activation/jakarta.activation
+#VERSION=1.2.1
+#sync_package
+
+#PACKAGE=org/jvnet/mimepull/mimepull
+#VERSION=1.9.11
+#sync_package
+
+#PACKAGE=org/jvnet/staxex/stax-ex
+#VERSION=1.8.1
+#sync_package
+#
+#PACKAGE=jakarta/xml/bind/jakarta.xml.bind-api
+#VERSION=2.3.2
+#sync_package
+
+#PACKAGE=org/apache/httpcomponents/httpclient-osgi
+#VERSION=4.5.10
+#sync_package
+
+# ----
+
 #PACKAGE=javax/xml/soap/javax.xml.soap-api
 #VERSION=1.4.0
 #sync_package
-
-PACKAGE=mysql/mysql-connector-java
-VERSION=5.1.48
-sync_package
-
-PACKAGE=biz/source_code/base64coder
-VERSION=/2010-12-19
-sync_package
-
-PACKAGE=javax/mail/mail
-VERSION=1.4.4
-sync_package
-
-PACKAGE=org/json/json
-VERSION=20190722
-sync_package
-
-PACKAGE=org/apache/derby/derby
-VERSION=10.14.2.0
-sync_package
-
-PACKAGE=commons-collections/commons-collections
-VERSION=3.2.1
-sync_package
-
-PACKAGE=jakarta/xml/soap/jakarta.xml.soap-api
-VERSION=1.4.1
-sync_package
-
-PACKAGE=com/sun/xml/messaging/saaj/saaj-impl
-VERSION=1.5.1
-sync_package
-
-PACKAGE=jakarta/activation/jakarta.activation-api
-VERSION=1.2.1
-sync_package
-
-PACKAGE=com/sun/activation/jakarta.activation
-VERSION=1.2.1
-sync_package
-
-PACKAGE=jakarta/xml/soap/jakarta.xml.soap-api
-VERSION=1.4.1
-sync_package
-
-PACKAGE=org/jvnet/mimepull/mimepull
-VERSION=1.9.11
-sync_package
-
-PACKAGE=org/jvnet/staxex/stax-ex
-VERSION=1.8.1
-sync_package
-
-PACKAGE=jakarta/xml/bind/jakarta.xml.bind-api
-VERSION=2.3.2
-sync_package
-
-PACKAGE=org/apache/httpcomponents/httpclient-osgi
-VERSION=4.5.10
-sync_package
 
 #PACKAGE=org/apache/servicemix/bundles/org.apache.servicemix.bundles.saaj-impl
 #VERSION=1.5.1_1
@@ -111,6 +110,7 @@ VERSION=
 
 
 cd ..
+rm -f ~/workspace/workspace/artifacts/artifacts/src/main/resources/obr/repository/jbundle-external-repository-$REPO_VERSION.xml
 java -jar $BINDEX -quiet -r ~/.m2/jbundle-external-repository.xml -t file:/home/don/.m2/%p/%f ~/temp/repository
 mv repository maven2
 java -jar $BINDEX -quiet -r ~/workspace/workspace/artifacts/artifacts/src/main/resources/obr/repository/jbundle-external-repository-$REPO_VERSION.xml -t https://repo.maven.apache.org/%p/%f  ~/temp/maven2
